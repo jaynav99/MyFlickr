ca977ce768da9adab2f087a87277eaf216d632ed
Jaynav Parikh <jaynav99@gmail.com>
2015-05-08 22:18:26 -0700

    MyFlicker
    Version 1

---
+++ app/src/main/java/com/example/jaynav/myflickr/Constants.java
@@ -0,0 +1,18 @@
+package com.example.jaynav.myflickr;
+
+/**
+ * Created by Jaynav on 5/7/2015.
+ */
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "3dafec5307ce267cb542be21d5f15881";
+    public static final String SECRET = "9c9acc2e88e8c630";
+    //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "1";
+
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+
+}
